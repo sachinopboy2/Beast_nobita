e592bf9e0ebd1140792d777b10466d51a8f3df12
olivamusic1 <aatha25u7@gmail.com>
2025-08-27 20:29:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheChampu/Nobita",
+    "https://github.com/olivamusic1/Shizuka_champu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
