877c0483cede53f159ec5e90b9ac524ad47ecb34
olivamusic1 <aatha25u7@gmail.com>
2025-08-27 20:55:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
 
 # API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 # VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-# API_KEY = getenv("API_KEY", '30DxNexGenBots935e08') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+# API_KEY = getenv("API_KEY", 'NxGBNexGenBots267382') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/akaChampu")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ShivanshuHUB")
