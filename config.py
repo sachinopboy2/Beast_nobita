f2a1d98533b3a1902009f64f1da96f862e9aead1
olivamusic1 <aatha25u7@gmail.com>
2025-08-27 21:48:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,8 +46,8 @@ API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
 API_KEY = getenv("API_KEY", 'NxGBNexGenBots267382') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/akaChampu")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ShivanshuHUB")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NOBITA_BOTS")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NOBITA_NETW0RK")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
